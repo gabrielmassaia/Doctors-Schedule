27f7c2691809e86256cd13f656b55cf682fcb2d1
gabrielmassaia <gabrielmassaia50@gmail.com>
2025-05-30 13:02:31 -0300

    feat: adiciona verificação de sessão e redirecionamento nas páginas de autenticação e dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,24 @@
-export default function Dashboard() {
-  return <div>Dashboard</div>;
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { auth } from "@/lib/auth";
+
+import { SignOutButton } from "./components/sign-out-button";
+export default async function Dashboard() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    redirect("/authentication");
+  }
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <h1>{session?.user?.email}</h1>
+      <h1>{session?.user?.name}</h1>
+      <SignOutButton />
+    </div>
+  );
 }
